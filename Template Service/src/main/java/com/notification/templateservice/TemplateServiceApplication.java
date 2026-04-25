23b64773a272ac550c49aed18026c27c53588786
Ramakrishna <ramkrishna.dhikonda@gmail.com>
2026-04-26 02:24:57 +0530

    Pushing missing project files

---
+++ Template Service/src/main/java/com/notification/templateservice/TemplateServiceApplication.java
@@ -0,0 +1,14 @@
+package com.notification.templateservice;
+
+import com.notification.templateservice.domain.NotificationTemplate;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TemplateServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TemplateServiceApplication.class, args);
+	}
+
+}
